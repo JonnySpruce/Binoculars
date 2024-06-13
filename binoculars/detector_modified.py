f1807e744b9b28b6211cefb24980b05b5757dfc6
AmyLaws <alaws@scottlogic.com>
2024-06-13 16:28:28 +0100

    refactored and changed approach

---
+++ binoculars/detector_modified.py
@@ -0,0 +1,138 @@
+from typing import Union
+
+import os
+import numpy as np
+import torch
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from .utils import assert_tokenizer_consistency
+from .metrics import perplexity, entropy
+
+torch.set_grad_enabled(False)
+
+huggingface_config = {
+    # Only required for private models from Huggingface (e.g. LLaMA models)
+    "TOKEN": os.environ.get("HF_TOKEN", None)
+}
+
+# selected using Falcon-7B and Falcon-7B-Instruct at bfloat16
+BINOCULARS_ACCURACY_THRESHOLD = 0.9015310749276843  # optimized for f1-score
+BINOCULARS_FPR_THRESHOLD = 0.8536432310785527  # optimized for low-fpr [chosen at 0.01%]
+
+DEVICE_1 = "cuda:0" if torch.cuda.is_available() else "cpu"
+DEVICE_2 = "cuda:1" if torch.cuda.device_count() > 1 else DEVICE_1
+
+
+class BinocularsModified(object):
+    def __init__(
+        self,
+        observer_name_or_path: str = "tiiuae/falcon-7b",
+        performer_name_or_path: str = "tiiuae/falcon-7b-instruct",
+        use_bfloat16: bool = True,
+        max_token_observed: int = 512,
+        mode: str = "low-fpr",
+    ) -> None:
+        assert_tokenizer_consistency(observer_name_or_path, performer_name_or_path)
+
+        self.change_mode(mode)
+        self.observer_model = AutoModelForCausalLM.from_pretrained(
+            observer_name_or_path,
+            device_map={"": DEVICE_1},
+            trust_remote_code=True,
+            torch_dtype=torch.bfloat16 if use_bfloat16 else torch.float32,
+            token=huggingface_config["TOKEN"],
+        )
+        self.performer_model = AutoModelForCausalLM.from_pretrained(
+            performer_name_or_path,
+            device_map={"": DEVICE_2},
+            trust_remote_code=True,
+            torch_dtype=torch.bfloat16 if use_bfloat16 else torch.float32,
+            token=huggingface_config["TOKEN"],
+        )
+        self.observer_model.eval()
+        self.performer_model.eval()
+
+        self.tokenizer = AutoTokenizer.from_pretrained(observer_name_or_path)
+        if not self.tokenizer.pad_token:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.max_token_observed = max_token_observed
+
+    def change_mode(self, mode: str) -> None:
+        if mode == "low-fpr":
+            self.threshold = BINOCULARS_FPR_THRESHOLD
+        elif mode == "accuracy":
+            self.threshold = BINOCULARS_ACCURACY_THRESHOLD
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
+
+    def _tokenize(self, batch: list[str]) -> transformers.BatchEncoding:
+        batch_size = len(batch)
+        encodings = self.tokenizer(
+            batch,
+            return_tensors="pt",
+            padding="longest" if batch_size > 1 else False,
+            truncation=True,
+            max_length=self.max_token_observed,
+            return_token_type_ids=False,
+        ).to(self.observer_model.device)
+        return encodings
+
+    @torch.inference_mode()
+    def _get_logits(self, encodings: transformers.BatchEncoding) -> torch.Tensor:
+        observer_logits = self.observer_model(**encodings.to(DEVICE_1)).logits
+        performer_logits = self.performer_model(**encodings.to(DEVICE_2)).logits
+        if DEVICE_1 != "cpu":
+            torch.cuda.synchronize()
+        return observer_logits, performer_logits
+
+    def compute_score(
+        self, input_text: str, context_length: int
+    ) -> Union[float, list[float]]:
+        context = input_text[:context_length]
+        code = input_text[context_length:]
+
+        context_encodings = self._tokenize(context)
+        code_encodings = self._tokenize(code)
+        code_and_context_encodings = self._tokenize(input_text)
+
+        context_logits_observer, context_logits_performer = self._get_logits(
+            context_encodings
+        )
+        code_logits_observer, code_logits_performer = self._get_logits(code_encodings)
+        context_and_code_logits_observer, context_and_code_logits_performer = (
+            self._get_logits(code_and_context_encodings)
+        )
+
+        _, code_only_logits_performer = torch.split(
+            context_and_code_logits_performer,
+            [context_logits_performer.size(dim=1), code_logits_performer.size(dim=1)],
+            dim=1,
+        )
+        _, code_only_logits_observer = torch.split(
+            context_and_code_logits_observer,
+            [context_logits_observer.size(dim=1), code_logits_observer.size(dim=1)],
+            dim=1,
+        )
+
+        ppl = perplexity(code_encodings, code_only_logits_performer)
+        x_ppl = entropy(
+            code_only_logits_observer.to(DEVICE_1),
+            code_only_logits_performer.to(DEVICE_1),
+            code_encodings.to(DEVICE_1),
+            self.tokenizer.pad_token_id,
+        )
+        binoculars_scores = ppl / x_ppl
+        binoculars_scores = binoculars_scores.tolist()
+        return (
+            binoculars_scores[0] if isinstance(input_text, str) else binoculars_scores
+        )
+
+    def predict(self, input_text: Union[list[str], str]) -> Union[list[str], str]:
+        binoculars_scores = np.array(self.compute_score(input_text))
+        pred = np.where(
+            binoculars_scores < self.threshold,
+            "Most likely AI-generated",
+            "Most likely human-generated",
+        ).tolist()
+        return pred
